8b31dfdbb26b369fc707cbbb901ac04a9ed6e3b7
ZhuMingHao <799573337@qq.com>
2016-10-11 13:55:49 +0800

    add CoffeePage

--- LittleAppication/View/CoffeePage.xaml.cs
+++ LittleAppication/View/CoffeePage.xaml.cs
@@ -22,9 +22,53 @@ namespace LittleAppication
     /// </summary>
     public sealed partial class CoffeePage : Page
     {
+        private string _roast;
+        private string _sweener;
+        private string _cream;
+
+
         public CoffeePage()
         {
             this.InitializeComponent();
+        }
+
+        private void Roasr_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = (MenuFlyoutItem)sender;
+            _roast = selected.Text;
+            displayResult();
+        }
+
+        private void Cream_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = (MenuFlyoutItem)sender;
+            _cream = selected.Text;
+            displayResult();
+        }
+
+        private void Sweetener_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = (MenuFlyoutItem)sender;
+            _sweener = selected.Text;
+            displayResult();
+        }
+        private void displayResult()
+        {if(_roast == "None" || string.IsNullOrEmpty(_roast))
+            {
+                ResulTextBlock.Text = "None";
+                return;
+            }
+            ResulTextBlock.Text = _cream +" "  + _sweener + " " + _roast;
+            if(_cream!= "None"&& !string.IsNullOrEmpty(_cream))
+            {
+                ResulTextBlock.Text += "+" + _cream;
+              
+            }
+            if (_sweener != "None" && !string.IsNullOrEmpty(_sweener))
+            {
+                ResulTextBlock.Text += "+" + _sweener;
+
+            }
         }
     }
 }
